6cdd25cf0ae72a1a9d4654f37c8dbac956471b2b
Arviuwu <arviu23@gmail.com>
2025-05-09 22:15:35 +0200

    Add project files.

---
+++ Dungeon RPG/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+namespace Dungeon_RPG
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
