e029bb10ac4b11c55c9b22bfa10ae8de6f67b983
Arviu <arviu23@gmail.com>
2025-05-10 16:35:15 +0200

    Basic Character Class with Stats

---
+++ Dungeon RPG/Model/Character.cs
@@ -0,0 +1,29 @@
+﻿namespace Dungeon_RPG.Model
+{
+    class Character
+    {
+        public int BaseHealth { get; set; }
+        public int Health { get; set; }
+        public int BaseMana { get; set; }
+        public int Strenght { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+        public decimal Money { get; set; }
+
+        // public Weapon Selected weapon
+        //Armor props
+        //inventory props
+        //stache
+
+        public Character()
+        {
+            BaseHealth = 100;
+            BaseMana = 100;
+            Money = 0;
+        }
+
+    }
+}
