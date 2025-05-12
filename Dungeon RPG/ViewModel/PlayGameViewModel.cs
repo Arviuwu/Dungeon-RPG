0df11016728fe1ff88eab55911b2357ae1fd7560
Arviu <arviu23@gmail.com>
2025-05-12 22:03:30 +0200

    Add CharacterDisplay and update UI styles
    - Introduced a new DataTemplate for CharacterDisplayViewModel in App.xaml.
    - Enhanced MenuButton style in GlobalStyles.xaml with FontWeight and FontSize.
    - Updated MainMenu.xaml buttons to utilize the new MenuButton style.
    - Modified PlayGame.xaml to include CharacterDisplay and apply MenuButton style to relevant buttons.
    - Refactored PlayGameViewModel to inherit from BaseViewModel and improved CharacterVM initialization.
    - Created CharacterDisplayViewModel with a default constructor.
    - Developed CharacterDisplay.xaml user control for character information display.
    - Implemented code-behind for CharacterDisplay.xaml.
    - Added tempCharacter.png image file.

--- Dungeon RPG/ViewModel/PlayGameViewModel.cs
+++ Dungeon RPG/ViewModel/PlayGameViewModel.cs
@@ -1,28 +1,32 @@
 ﻿using Dungeon_RPG.Model;
+using Dungeon_RPG.MVVM;
 using Dungeon_RPG.Services;
 using Dungeon_RPG.Stores;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dungeon_RPG.ViewModel
 {
-    public class PlayGameViewModel
+    public class PlayGameViewModel : BaseViewModel
     {
         private readonly INavigationService _navigation;
+        
         public CharacterStore CharacterStore { get; set; }
         public CharacterViewModel CharacterVM { get; }
         public PlayGameViewModel( INavigationService navigation, CharacterStore characterStore)
         {
             _navigation = navigation;
             CharacterStore = characterStore;
-            CharacterVM = new(CharacterStore.CurrentCharacter);
-            foreach (Stat stat in CharacterStore.CurrentCharacter.AllStats)
+            CharacterVM = new(CharacterStore.CurrentCharacter!);
+            foreach (Stat stat in CharacterStore.CurrentCharacter!.AllStats)
             {
                 CharacterVM.AllStats.Add(new StatViewModel(stat, CharacterVM));
             }
+            
         }
     }
 }
