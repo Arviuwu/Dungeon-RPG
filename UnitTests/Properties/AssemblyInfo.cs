47b18eede1670b16da2deafd1f34197aa895c498
Arviu <arviu23@gmail.com>
2025-06-13 17:15:48 +0200

    Add unit testing project and configurations
    - Created a new "UnitTests" project in the solution.
    - Added `packages.config` to manage NuGet packages for MSTest and xUnit.
    - Implemented a basic test class `UnitTest1.cs` with a test method.
    - Defined project settings in `UnitTests.csproj`, including necessary package references.
    - Added `AssemblyInfo.cs` for assembly metadata of the unit tests.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8cfcdefb-bd5f-4710-94c9-aeb7c17cd43e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
